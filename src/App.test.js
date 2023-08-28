feaffc82a1e9ef65626c9ad312d4ede17068bf28
patibandalalithphanindra <patibandalp01@gmail.com>
2023-08-28 11:48:29 +0530

    initial test

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import { render, screen, fireEvent} from '@testing-library/react';
+import App from './App';
+
+  test('button has correct initial color and updates when clicked',() => {
+    render(<App/>);
+    const colorButton = screen.getByRole('button', { name : 'Change to blue'});
+    expect(colorButton).toHaveStyle({backgroundColor : 'red'});
+    fireEvent.click(colorButton);
+    expect(colorButton).toHaveStyle({backgroundColor : 'blue'});
+    expect(colorButton).toHaveTextContent('Change to red');
+  });

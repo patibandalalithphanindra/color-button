a0f538fda0bdf52a2afd62dff71ab821d5c5dd8e
patibandalalithphanindra <patibandalp01@gmail.com>
2023-08-29 11:40:00 +0530

    eslint dependencies

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 export function replaceCamelWithCases(colorName) {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
 }
 
-
 function App() {
-  const [buttonColor, setButtonColor] = useState('red');
+  const [buttonColor, setButtonColor] = useState("red");
   const [disabled, setDisabled] = useState(false);
-  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   return (
     <div>
-      <button 
-      style={{backgroundColor : disabled ? 'gray' : buttonColor}}
-      onClick={() => setButtonColor(newButtonColor)}
-      disabled={disabled}>
+      <button
+        style={{ backgroundColor: disabled ? "gray" : buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
         Change to {newButtonColor}
       </button>
-      <input 
-      type="checkbox" 
-      onChange={(e)=> {setDisabled(e.target.checked)}}
-      defaultChecked={disabled}
-      id = "enable-button-checkbox"
+      <input
+        type="checkbox"
+        onChange={(e) => {
+          setDisabled(e.target.checked);
+        }}
+        defaultChecked={disabled}
+        id="enable-button-checkbox"
       />
-      <label htmlFor='enable-button-checkbox'>Disable button</label>
+      <label htmlFor="enable-button-checkbox">Disable button</label>
     </div>
   );
 }
